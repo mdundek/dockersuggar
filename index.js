0ffc899b1368df898497993fdf86e778d0003686
mdundek <michael.dundek@fr.ibm.com>
2018-05-09 12:17:19 +0200

    Version increment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var figlet = require('figlet');
 
 
 program
-    .version('0.0.9')
+    .version('0.1.0')
     .description("\n" + figlet.textSync('DockerSuggar'))
     .option('-r, --remote <name>', 'Execute command on a remote docker instance');
 
