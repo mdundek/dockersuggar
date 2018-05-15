b0ac8b5a38d920c95607f155297dfdcc2fe729b9
mdundek <michael.dundek@fr.ibm.com>
2018-05-15 21:54:13 +0200

    Readme bug fix and version bump

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var chalk = require("chalk");
 var figlet = require('figlet');
 
 program
-    .version('0.1.4')
+    .version('0.1.5')
     .description("\n" + figlet.textSync('DockerSuggar'))
     .option('-r, --remote <name>', 'Execute command on a remote docker instance');
 
